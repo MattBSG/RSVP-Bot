1652907d6daab9cdb3d2c179b111fa629ca15908
Matthew Cohen <matt@mattbsg.xyz>
2020-08-03 21:08:48 -0400

    Update lang and add new constants

--- constants.py
+++ constants.py
@@ -10,8 +10,8 @@ DISCORD_TOKEN = 'token'
 DISCORD_PREFIX = '?'
 
 # Set aliases for timezones. All but last line should end in a comma
-# The key (i.e. eastern) is the user passed argument
-# while the value (i.e. 'America/New_York') should a TZ data.
+# The key (i.e. eastern) is the user passed argument while the value
+# (i.e. 'America/New_York') should be a TZ data. Do not use spaces or uppercase in the alias.
 # Find a TZ name for a region here: http://www.timezoneconverter.com/cgi-bin/findzone.tzc
 TIMEZONE_ALIASES = {
     'eastern': 'America/New_York',
@@ -27,3 +27,28 @@ TIMEZONE_ALIASES = {
 #  ONLY EDIT IF YOU KNOW WHAT YOU ARE DOING  #
 ##############################################
 MONGO_URI = 'mongodb://root:rsvpbot@db:27017/?authSource=admin'
+import pendulum
+DAYS = {
+    0: pendulum.SUNDAY,
+    1: pendulum.MONDAY,
+    2: pendulum.TUESDAY,
+    3: pendulum.WEDNESDAY,
+    4: pendulum.THURSDAY,
+    5: pendulum.FRIDAY,
+    6: pendulum.SATURDAY
+}
+DAY_MAPPING = {
+    'sunday': 0,
+    'monday': 1,
+    'tuesday': 2,
+    'wednesday': 3,
+    'thursday': 4,
+    'friday': 5,
+    'saturday': 6,
+}
+STATUS_MAPPING = {
+    'host': EMOJI_LEADER,
+    'confirmed': EMOJI_CONFIRMED,
+    'tentative': EMOJI_TENTATIVE,
+    'late': EMOJI_LATE
+}
