679238a578b60507f59fed47d9a5e95211a860a3
Matthew Cohen <matt@mattbsg.xyz>
2020-08-28 16:13:42 -0400

    Nevermind

--- constants.py
+++ constants.py
@@ -46,15 +46,6 @@ DAY_MAPPING = {
     'friday': 5,
     'saturday': 6,
 }
-INV_DAY_MAPPING = {
-    0: 'sunday',
-    1: 'monday',
-    2: 'tuesday',
-    3: 'wednesday',
-    4: 'thursday',
-    5: 'friday',
-    6: 'saturday',
-}
 STATUS_MAPPING = {
     'host': EMOJI_LEADER,
     'confirmed': EMOJI_CONFIRMED,
